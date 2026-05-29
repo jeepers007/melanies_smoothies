86a0956e06f2bd869fb2311246f24aaf09b1276d
jeepers007 <jp.daily@comcast.net>
2026-05-29 07:43:19 -0700

    Update streamlit_app.py
    Moved request to bottom of form and added json parsing

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response)
+
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw: {st.__version__}")
@@ -37,3 +36,6 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+      
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+st.text(smoothiefroot_response.json())
